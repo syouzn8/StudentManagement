8812f5c691d3586756d972d809965a0d6e1fb13d
uehara <shangyuanshouya@gmail.com>
2024-08-05 23:00:23 +0900

    syokaicommit

---
+++ src/main/java/raisetech/StudentManagement/data/Student.java
@@ -0,0 +1,25 @@
+package raisetech.StudentManagement.data;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Student {
+
+  private String id;
+  private String name;
+  private String nickname;
+  private String email;
+  private String town;
+  private String gender;
+  private int age;
+  private String furigana;
+  private String remark;
+  private boolean isDeleted;
+
+
+
+
+
+}
